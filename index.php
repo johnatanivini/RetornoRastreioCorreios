fb7dc512fb2a8181296b003f99cc03def02e7fac
fabricainfo <fabricainfo>
2016-01-28 13:28:41 -0300

    Class RetornoCorreio

--- index.php
+++ index.php
@@ -1,37 +1,5 @@
 <?php 
-header("Content-type:text/html; charset=utf-8");
-/**
-** Aqui se usa a library simple_html_dom.php para
-** percorrer o html do retorno dos correios para buscar o ultimo status
-** do código de rastreamento
-** por enquanto ainda é funcional
-**/ 
-include("simple_html_dom.php");
-$codigo = "PE039373771BR";
-$url='http://websro.correios.com.br/sro_bin/txect01$.Inexistente?P_LINGUA=001&P_TIPO=002&P_COD_LIS=' . $codigo;
-//// Função do Simple Html Dom retorna uma instancia.
-$dom = file_get_html($url);
-///busca somente as linhas das tabelas
-$html = $dom->find("tr");
-/**
-* Possíveis situações de retorno 
-* para uso em demais eventos
-**/
-$situacao = array(
-'entrega efetuada',
-'saiu  para entrega ao destinatário',
-'encaminhado',
-'postado'
-);
-/// Hora Atual 
-$hora_atual = strtolower($html[1]->find('td',0)->plaintext);
-//// Local Atual
-$local_atual = strtolower($html[1]->find('td',1)->plaintext);
-/// quero apenas a terceira coluna ( sem loop), tudo minuscula
-$situacao_atual = strtolower($html[1]->find('td',2)->plaintext);
-/// Situação atual
-echo $hora_atual." / ".$local_atual." / ".$situacao_atual;
-
-
-
-
+require('retorno_correios.class.php');
+$obj= new RetornoCorreio();
+$obj->setCodigo('PI287643179BR');
+$obj->statusRastreamento();
---
+++ retorno_correios.class.php
@@ -0,0 +1,63 @@
+<?php 
+header("Content-type:text/html; charset=utf-8");
+/**
+** Aqui se usa a library simple_html_dom.php para
+** percorrer o html do retorno dos correios para buscar o ultimo status
+** do código de rastreamento
+** por enquanto ainda é funcional
+**/ 
+include("simple_html_dom.php");
+class RetornoCorreios extends simple_html_dom{
+    
+    private $codigo = "PI287643179BR";
+    public $url ="http://websro.correios.com.br/sro_bin/txect01$.QueryList?P_TIPO=001&P_LINGUA=001&P_COD_UNI=";
+
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function statusRastreamento(){
+    
+       
+        $dom = file_get_html("{$this->getUrl()}");
+        $html = $dom->find('tr');
+        if($html){
+        /// Hora Atual
+        $hora_atual = strtolower($html[1]->find('td',0)->plaintext);
+        //// Local Atual
+        $local_atual = strtolower($html[1]->find('td',1)->plaintext);
+        /// quero apenas a terceira coluna ( sem loop), tudo minuscula
+        $situacao_atual = strtolower($html[1]->find('td',2)->plaintext);
+        /// Situação atual
+        
+        return $hora_atual." / ".$local_atual." / ".$situacao_atual;
+        }else{
+            $html = $dom->find('font',1)->plaintext;
+            return $html;
+        }
+        
+            
+ 
+        
+
+    }
+    function getCodigo() {
+        return $this->codigo;
+    }
+
+    function setCodigo($codigo) {
+        $this->codigo = $codigo;
+    }
+
+
+    function getUrl() {
+        return $this->url.$this->codigo;
+    }
+
+}
+
+
+
+
+
+
